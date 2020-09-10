5967cf22b015980b6886c95c1f6aa9dbd7eb3d88
Mitesh Parsrampuria <mitesh2405@gmail.com>
2020-09-10 17:49:23 +0530

    Setup app to add to Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const geocode = require('./utils/geocode')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -79,6 +80,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("Server is up at port 3000")
+app.listen(port, () => {
+    console.log("Server is up at port " + port)
 })
